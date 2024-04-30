b5b381b3133f40f49a2a73bba1b8f6a8bc013911
taha <taha68496@gmail.com>
2024-04-30 05:42:24 -0700

    first

---
+++ index.js
@@ -0,0 +1,46 @@
+import inquirer from "inquirer";
+let currencyconverter = {
+    "USD": {
+        "USD": 1,
+        "AED": 3.67,
+        "PKR": 278
+    },
+    "AED": {
+        "USD": 1.08,
+        "AED": 1,
+        "PKR": 75.81
+    },
+    "PKR": {
+        "USD": 0.0036,
+        "AED": 0.013,
+        "PKR": 1
+    },
+};
+const answer = await inquirer.prompt([
+    {
+        name: "from",
+        message: "Enter your Currency",
+        type: "list",
+        choices: ["USD", "AED", "PKR"]
+    },
+    {
+        name: "to",
+        message: "Select which currency convert?",
+        type: "list",
+        choices: ["USD", "AED", "PKR"]
+    },
+    {
+        name: "amount",
+        message: "Enter your amount in number",
+        type: "number",
+        choices: ["USD", "AED", "PKR"]
+    }
+]);
+const { from, to, amount } = answer;
+if (from && to && amount) {
+    let result = currencyconverter[from][to] * amount;
+    console.log(`Your conversion from ${from} to ${to} is ${result}`);
+}
+else {
+    console.log("invalid operator select");
+}
